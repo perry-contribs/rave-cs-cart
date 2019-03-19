dd5b9d1c54485f67e5b0e96772c44a08c933621b
temi adesina <adesinatemiloluwa@gmail.com>
2019-03-19 02:30:56 +0100

    rave official cs-cart plugin

---
+++ app/payments/rave.php
@@ -0,0 +1,223 @@
+<?php
+use Tygh\Registry;
+
+function fn_rave_adjust_amount($price, $payment_currency)
+{
+    $currencies = Registry::get('currencies');
+
+    if (array_key_exists($payment_currency, $currencies)) {
+        if ($currencies[$payment_currency]['is_primary'] != 'Y') {
+            $price = fn_format_price($price / $currencies[$payment_currency]['coefficient']);
+        }
+    } else {
+        return false;
+    }
+
+    return $price;
+}
+
+function fn_rave_place_order($original_order_id)
+{
+    $cart = &$_SESSION['cart'];
+    $auth = &$_SESSION['auth'];
+
+    list($order_id, $process_payment) = fn_place_order($cart, $auth);
+
+    $data = array(
+        'order_id' => $order_id,
+        'type' => 'S',
+        'data' => TIME,
+    );
+    db_query('REPLACE INTO ?:order_data ?e', $data);
+
+    $data = array(
+        'order_id' => $order_id,
+        'type' => 'E', // extra order ID
+        'data' => $original_order_id,
+    );
+    db_query('REPLACE INTO ?:order_data ?e', $data);
+
+    return $order_id;
+}
+
+
+
+
+// if (!defined('BOOTSTRAP')) {
+//     die('Access denied');
+// }
+
+// Return from payment
+if (!empty($_REQUEST['txref'])) {
+// if ($mode == 'return' && !empty($_REQUEST['txref'])) {
+    require './init_payment.php';
+    if (isset($view) === false) {
+        $view = Registry::get('view');
+    }
+
+        // $view->assign('order_action', __('placing_order'));
+        // $view->display('views/orders/components/placing_order.tpl');
+        // fn_flush();
+    $payment_id = db_get_field("SELECT ?:payments.payment_id FROM ?:payments LEFT JOIN ?:payment_processors ON ?:payment_processors.processor_id = ?:payments.processor_id WHERE ?:payment_processors.processor_script = 'rave.php'");
+    $processor_data = fn_get_payment_method_data($payment_id);
+    $ref = $_REQUEST['txref'];
+    $order_id = fn_rave_place_order(explode("_", $_REQUEST['txref'])[0]);
+    $amount = db_get_field("SELECT ?:orders.total FROM ?:orders  WHERE ?:orders.order_id = " . $order_id);
+    // $currency = $processor_data['processor_params']['rave_currency'];
+    $currency = fn_get_secondary_currency();
+    $currencyamount = fn_rave_adjust_amount($amount, $currency);
+
+    if (!empty($order_id) and !empty($currencyamount)) {
+        if (fn_check_payment_script('rave.php', $order_id, $processor_data)) {
+            $mode = $processor_data['processor_params']['rave_mode'];
+            if ($mode == 'test') {
+                $url = "https://ravesandboxapi.flutterwave.com";
+                $PBFPubKey = $processor_data['processor_params']['rave_tpk'];
+                $secretKey = $processor_data['processor_params']['rave_tsk'];
+            } else {
+                $url = "https://api.ravepay.co";
+                $PBFPubKey = $processor_data['processor_params']['rave_lpk'];
+                $secretKey = $processor_data['processor_params']['rave_lsk'];
+            }
+            $query = array(
+                "SECKEY" => $secretKey,
+                "txref" => $ref
+            );
+
+            $data_string = json_encode($query);
+
+            $ch = curl_init($url . '/flwv3-pug/getpaidx/api/v2/verify');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+            $response = curl_exec($ch);
+
+            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $header = substr($response, 0, $header_size);
+            $body = substr($response, $header_size);
+
+            curl_close($ch);
+
+            if (curl_errno($ch)) {   // should be 0
+                    // curl ended with an error
+                $cerr = curl_error($ch);
+                curl_close($ch);
+                throw new Exception("Curl failed with response: '" . $cerr . "'.");
+            }
+
+            $resp = json_decode($response, true);
+
+            $paymentStatus = $resp['data']['status'];
+            $chargeResponsecode = $resp['data']['chargecode'];
+            $chargeAmount = $resp['data']['amount'];
+            $chargeCurrency = $resp['data']['currency'];
+            $code = $resp['data']['txref'];
+
+            if (($chargeResponsecode == "00" || $chargeResponsecode == "0") && ($chargeAmount == $currencyamount) && ($chargeCurrency == $currency)) {
+                $pp_response['order_status'] = 'P';
+                $pp_response['transaction_id'] = $code;
+                $pp_response['Status'] = 'Payment Successful';
+                    // fn_change_order_status($order_id, 'P');
+                fn_finish_payment($order_id, $pp_response, false);
+                fn_order_placement_routines('route', $order_id, false);
+            } else {
+                $pp_response['order_status'] = 'O';
+                $pp_response['transaction_id'] = $code;
+                $pp_response['Status'] = 'Payment Failed';
+
+                    // fn_finish_payment($order_id, $pp_response);
+                fn_set_notification('E', __('error'), 'Payment Failed #' . $code);
+                fn_order_placement_routines('checkout_redirect');
+            }
+
+        }
+    } else {
+        fn_set_notification('E', __('error'), 'Payment Unsuccessful' . $_REQUEST['merchant_order_id']);
+        fn_order_placement_routines('checkout_redirect');
+    }
+
+    exit;
+} else {
+    defined('BOOTSTRAP') or die('Access denied');
+    $mode = $processor_data['processor_params']['rave_mode'];
+    if ($mode == 'test') {
+        $url = "https://ravesandboxapi.flutterwave.com";
+        $PBFPubKey = $processor_data['processor_params']['rave_tpk'];
+        $secretKey = $processor_data['processor_params']['rave_tsk'];
+        $env = "staging";
+    } else {
+        $url = "https://api.ravepay.co";
+        $PBFPubKey = $processor_data['processor_params']['rave_lpk'];
+        $secretKey = $processor_data['processor_params']['rave_lsk'];
+        $env = "live";
+    }
+
+    // $total = fn_rave_adjust_amount($order_info['total'], $processor_data['processor_params']['rave_currency']);
+    $total = $order_info['total'] + $order_info['payment_surcharge'];
+    $paymentMethod = $processor_data['processor_params']['rave_payment_method'];
+    // $country = $processor_data['processor_params']['rave_country'];
+    $logo = $processor_data['processor_params']['rave_logo'];
+    // $currency = $processor_data['processor_params']['rave_currency'];
+    $currency = fn_get_secondary_currency();
+    $email = $order_info['email'];
+    $firstName = $order_info['b_firstname'];
+    $lastName = $order_info['b_lastname'];
+    $phone = $order_info['phone'];
+    $ref = $order_id;
+
+    if ($currency != "NGN") {
+        $paymentMethod = "both";
+    }
+
+    // switch country based on currency
+    switch ($currency) {
+        case 'KES':
+          $country = 'KE';
+          break;
+        case 'GHS':
+          $country = 'GH';
+          break;
+        case 'ZAR':
+          $country = 'ZA';
+          break;
+        default:
+          $country = 'NG';
+          break;
+    }
+
+    $html = '<!DOCTYPE html>
+  <html>
+  <body>
+
+    <form method="POST" action="https://hosted.flutterwave.com/processPayment.php" id="paymentForm">
+        <input type="hidden" name="amount" value="' . $total . '" >
+        <input type="hidden" name="payment_method" value="' . $paymentMethod . '" >
+        <input type="hidden" name="logo" value="' . $logo . '" >
+        <input type="hidden" name="country" value="' . $country . '" >
+        <input type="hidden" name="currency" value="' . $currency . '" >
+        <input type="hidden" name="email" value="' . $email . '" >
+        <input type="hidden" name="firstname" value="' . $firstName . '" >
+        <input type="hidden" name="lastname"value="' . $lastName . '" >
+        <input type="hidden" name="phonenumber" value="' . $phone . '" >
+        <input type="hidden" name="ref" id="ref" value="' . $ref . '_' . time() . '" >
+        <input type="hidden" name="env" value="' . $env . '">
+        <input type="hidden" name="publicKey" value="' . $PBFPubKey . '">
+        <input type="hidden" name="secretKey" value="' . $secretKey . '">
+        <input type="hidden" name="successurl" value="' . fn_payment_url('current', 'rave.php') . '">
+        <input type="hidden" name="failureurl" value="' . fn_payment_url('current', 'rave.php') . '">
+    </form>
+    <script>
+        window.onload = function(){
+            document.forms["paymentForm"].submit();
+        }
+    </script>
+    </body>
+    </html>';
+
+    echo $html;
+}
+
+?>
---
+++ design/backend/templates/views/payments/components/cc_processors/rave.tpl
@@ -0,0 +1,82 @@
+<div class="control-group">
+    <label class="control-label" for="rave_tpk_{$payment_id}">Test Public Key:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][rave_tpk]" id="rave_tpk_{$payment_id}" value="{$processor_params.rave_tpk}"   size="60">
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="rave_tsk_{$payment_id}">Test Secret Key:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][rave_tsk]" id="rave_tsk_{$payment_id}" value="{$processor_params.rave_tsk}"   size="60">
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="rave_lpk_{$payment_id}">Live Public Key:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][rave_lpk]" id="rave_lpk_{$payment_id}" value="{$processor_params.rave_lpk}"   size="60">
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="rave_lsk_{$payment_id}">Live Secret Key:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][rave_lsk]" id="rave_lsk_{$payment_id}" value="{$processor_params.rave_lsk}"   size="60">
+    </div>
+</div>
+{* <input type="hidden" name="payment_data[processor_params][iframe_mode]" value="Y"   size="60"> *}
+
+{* <div class="control-group form-field">
+    <label class="control-label" for="rave_country_{$payment_id}">Country:</label>
+    <div class="controls">
+      <select name="payment_data[processor_params][rave_country]" id="rave_country_{$payment_id}">
+          <option value="NG"{if $processor_params.rave_country == "NG"}selected="selected"{/if}>Nigeria</option>
+          <option value="GH" {if $processor_params.rave_country == "GH"}selected="selected"{/if}>Ghana</option>
+          <option value="ZA" {if $processor_params.rave_country == "ZA"}selected="selected"{/if}>South Africa</option>
+          <option value="KE" {if $processor_params.rave_country == "KE"}selected="selected"{/if}>Kenya</option>
+      </select>
+    </div>
+</div> *}
+
+{* <div class="control-group form-field">
+    <label class="control-label" for="rave_currency_{$payment_id}">Currency:</label>
+    <div class="controls">
+      <select name="payment_data[processor_params][rave_currency]" id="rave_currency_{$payment_id}">
+          <option value="NGN"{if $processor_params.rave_currency == "NGN"}selected="selected"{/if}>Naira</option>
+          <option value="USD" {if $processor_params.rave_currency == "USD"}selected="selected"{/if}>US Dollars</option>
+          <option value="EUR" {if $processor_params.rave_currency == "EUR"}selected="selected"{/if}>Euros</option>
+          <option value="GBP" {if $processor_params.rave_currency == "GBP"}selected="selected"{/if}>Pounds Sterling</option>
+          <option value="GHS" {if $processor_params.rave_currency == "GHS"}selected="selected"{/if}>Ghana Cedis</option>
+          <option value="KES" {if $processor_params.rave_currency == "KES"}selected="selected"{/if}>Kenya Shillings</option>
+          <option value="ZAR" {if $processor_params.rave_currency == "ZAR"}selected="selected"{/if}>South African Rands</option>
+      </select>
+    </div>
+</div> *}
+
+<div class="control-group form-field">
+    <label class="control-label" for="rave_payment_method_{$payment_id}">Payment Method:</label>
+    <div class="controls">
+      <select name="payment_data[processor_params][rave_payment_method]" id="rave_payment_method_{$payment_id}">
+          <option value="both"{if $processor_params.rave_payment_method == "both"}selected="selected"{/if}>All</option>
+          <option value="card" {if $processor_params.rave_payment_method == "card"}selected="selected"{/if}>Cards only</option>
+          <option value="account" {if $processor_params.rave_payment_method == "account"}selected="selected"{/if}>Account Only</option>
+          <option value="ussd" {if $processor_params.rave_payment_method == "ussd"}selected="selected"{/if}>USSD only</option>
+      </select>
+    </div>
+</div>
+
+<div class="control-group">
+    <label class="control-label" for="rave_logo_{$payment_id}">Logo URL:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][rave_logo]" id="rave_logo_{$payment_id}" value="{$processor_params.rave_logo}" >
+    </div>
+</div>
+
+<div class="control-group form-field">
+    <label class="control-label" for="iframe_mode_{$payment_id}">Mode:</label>
+    <div class="controls">
+      <select name="payment_data[processor_params][rave_mode]" id="iframe_mode_{$payment_id}">
+          <option value="test"{if $processor_params.rave_mode == "test"}selected="selected"{/if}>Test</option>
+          <option value="live" {if $processor_params.rave_mode == "live"}selected="selected"{/if}>Live</option>
+      </select>
+    </div>
+
+</div>
